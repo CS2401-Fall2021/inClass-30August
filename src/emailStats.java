0e13d29bad2a0845780823a7d2d2ae171c638183
Dan DeBlasio <dan@dandeblasio.com>
2021-08-30 13:02:44 -0600

    Adding fake emails

--- src/emailStats.java
+++ src/emailStats.java
@@ -1,5 +1,9 @@
 public class emailStats{
-    public static void main(String[] args){
+  public static void main(String[] args){
       
-    }
+  }
+    
+  public static String[] produceRandomEmails() {
+    return new String[] {"kramulous@optonline.net","rhavyn@iCloud.com","jgwang@gmail.com","xtang@outlook.com","tubesteak@outlook.com","airship@att.net","pgolle@live.com","staffelb@comcast.net","techie@verizon.net","tedrlord@Hotmail.com","osaru@hotmail.com","nwiger@verizon.net","bahwi@att.net","enintend@outlook.com","webteam@outlook.com","martink@mac.com","paley@yahoo.com","heine@gmail.com","podmaster@aol.com","kdawson@me.com","cyrus@mac.com","mleary@verizon.net","philen@att.net","neuffer@hotmail.com","elflord@yahoo.ca","jesse@Icloud.com","ramollin@msn.com","thaljef@me.com","osrin@me.com","sacraver@hotmail.com","spadkins@att.net","pereinar@verizon.net","jguyer@icloud.com","sethbrown@icloud.com","calin@hotmail.com","kdawson@comcast.net","jfriedl@live.com","miami@live.com","sacraver@mac.com","fukuchi@outlook.com","hstiles@comcast.net","chrwin@icloud.com","osrin@yahoo.ca","daveed@live.com","itstatus@yahoo.com","pkplex@yahoo.com","johndo@sbcglobal.net","munjal@aol.com","mdielmann@msn.com","sriha@icloud.com"};
+  }
 }
